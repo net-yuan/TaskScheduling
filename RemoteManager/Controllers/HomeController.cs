607b30ec095cbeb519f3cb413acf25e4cb040f4d
anangyang <anyanghappiness@163.com>
2018-12-14 10:11:47 +0800

    Edit

--- RemoteManager/Controllers/HomeController.cs
+++ RemoteManager/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace WebApplication1.Controllers
+namespace RemoteManager.Controllers
 {
     public class HomeController : Controller
     {
--- RemoteManager/Properties/AssemblyInfo.cs
+++ RemoteManager/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息是通过以下项进行控制的
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("WebApplication1")]
+[assembly: AssemblyTitle("RemoteManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApplication1")]
+[assembly: AssemblyProduct("RemoteManager")]
 [assembly: AssemblyCopyright("版权所有(C)  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
